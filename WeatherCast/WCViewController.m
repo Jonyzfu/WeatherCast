acd14f8ffca6a7fc0ee33958e2a442d6f98b007f
Jonyzfu <Jonyzfu@users.noreply.github.com>
2014-08-04 17:54:01 -0700

    Display Data in the Table View
    Add scroll delegate to improve the blur effect.

--- WeatherCast/WCViewController.m
+++ WeatherCast/WCViewController.m
@@ -16,6 +16,8 @@
 @property(nonatomic, strong) UIImageView *blurredImageView;
 @property(nonatomic, strong) UITableView *tableView;
 @property(nonatomic, assign) CGFloat screenHeight;
+@property(nonatomic, strong) NSDateFormatter *hourlyFormatter;
+@property(nonatomic, strong) NSDateFormatter *dailyFormatter;
 
 @end
 
@@ -26,6 +28,20 @@
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
+    }
+    return self;
+}
+
+
+// Placing them in -init you’ll ensure they’re initialized only once by your view controller.
+- (id)init
+{
+    if (self = [super init]) {
+        _hourlyFormatter = [[NSDateFormatter alloc] init];
+        _hourlyFormatter.dateFormat = @"h a";
+        
+        _dailyFormatter = [[NSDateFormatter alloc] init];
+        _dailyFormatter.dateFormat = @"EEEE";
     }
     return self;
 }
@@ -165,6 +181,18 @@
          iconView.image = [UIImage imageNamed:[newCondition imageName]];
      }];
     
+    [[RACObserve([WCManager sharedManager], hourlyForecast)
+      deliverOn:RACScheduler.mainThreadScheduler]
+     subscribeNext:^(NSArray *newForecast) {
+         [self.tableView reloadData];
+     }];
+    
+    [[RACObserve([WCManager sharedManager], dailyForecast)
+      deliverOn:RACScheduler.mainThreadScheduler]
+     subscribeNext:^(NSArray *newForecast) {
+         [self.tableView reloadData];
+     }];
+    
     
     [[WCManager sharedManager] findCurrentLocation];
     
@@ -190,10 +218,25 @@
     return 2;
 }
 
+
+// The table view is set up with paging enabled and sticky-scrolling behavior would look odd in this context.
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    // TODO: Return count of forecast
-    return 0;
+    // The first section is for the hourly forecast.
+    if (section == 0) {
+        return MIN([[WCManager sharedManager].hourlyForecast count], 6) + 1;
+    }
+    
+    // The next section is for daily forecasts.
+    return MIN([[WCManager sharedManager].dailyForecast count], 6) + 1;
+}
+
+
+// This divides the screen height by the number of cells in each section so the total height of all cells equals the height of the screen.
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSInteger cellCount = [self tableView:tableView numberOfRowsInSection:indexPath.section];
+    return self.screenHeight / (CGFloat)cellCount;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -211,10 +254,59 @@
     cell.textLabel.textColor = [UIColor whiteColor];
     cell.detailTextLabel.textColor = [UIColor whiteColor];
     
-    // TODO: Setup the cell
-    
+    if (indexPath.section == 0) {
+        // The first row of each section is the header cell.
+        if (indexPath.row == 0) {
+            [self configureHeaderCell:cell title:@"Hourly Forecast"];
+        } else {
+            // Get the hourly weather and configure the cell using custom configure methods.
+            WCCondition *weather = [WCManager sharedManager].hourlyForecast[indexPath.row - 1];
+            [self configureHourlyCell:cell weather: weather];
+        }
+    } else if (indexPath.section == 1) {
+        if (indexPath.row == 0) {
+            [self configureHeaderCell:cell title:@"Daily Forecast"];
+        } else {
+            // Get the daily weather and configure the cell using another custom configure method.
+            WCCondition *weather = [WCManager sharedManager].dailyForecast[indexPath.row - 1];
+            [self configureDailyCell:cell weather: weather];
+        }
+    }
     return cell;
 }
+
+// Configures and adds text to the cell used as the section header.
+- (void)configureHeaderCell:(UITableViewCell *)cell title:(NSString *)title {
+    cell.textLabel.font = [UIFont fontWithName:@"HelveticaNeue-Medium" size:18];
+    cell.textLabel.text = title;
+    cell.detailTextLabel.text = @"";
+    cell.imageView.image = nil;
+}
+
+// Formats the cell for an hourly forecast.
+- (void)configureHourlyCell:(UITableViewCell *)cell weather:(WCCondition *)weather {
+    cell.textLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18];
+    cell.detailTextLabel.font = [UIFont fontWithName:@"HelveticaNeue-Medium" size:18];
+    cell.textLabel.text = [self.hourlyFormatter stringFromDate:weather.date];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%.0f°",weather.temperature.floatValue];
+    cell.imageView.image = [UIImage imageNamed:[weather imageName]];
+    cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
+}
+
+// Formats the cell for a daily forecast.
+- (void)configureDailyCell:(UITableViewCell *)cell weather:(WCCondition *)weather {
+    cell.textLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:18];
+    cell.detailTextLabel.font = [UIFont fontWithName:@"HelveticaNeue-Medium" size:18];
+    cell.textLabel.text = [self.dailyFormatter stringFromDate:weather.date];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%.0f° / %.0f°",
+                                 weather.tempHigh.floatValue,
+                                 weather.tempLow.floatValue];
+    cell.imageView.image = [UIImage imageNamed:[weather imageName]];
+    cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
+}
+
+
+
 
 -(void)viewWillLayoutSubviews
 {
@@ -225,6 +317,22 @@
     self.tableView.frame = bounds;
 }
 
+#pragma mark - UIScrollViewDelegate
+
+// The blur should fill in dynamically as you scroll past the first page of forecast.
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView
+{
+    // Get the height of the scroll view and the content offset.
+    // Cap the offset at 0 so attempting to scroll past the start of the table won’t affect blurring.
+    CGFloat height = scrollView.bounds.size.height;
+    CGFloat position = MAX(scrollView.contentOffset.y, 0.0);
+    
+    // Divide the offset by the height with a maximum of 1 so that your offset is capped at 100%.
+    CGFloat percent = MIN(position / height, 1.0);
+    
+    // Assign the resulting value to the blur image’s alpha property to change how much of the blurred image you’ll see as you scroll.
+    self.blurredImageView.alpha = percent;
+}
 
 
 /*
